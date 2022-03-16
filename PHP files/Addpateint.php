ceb46cf066d8adb142e0fa7cd764fa81dcb63aae
Samrat Abdul Jalil <95965521+Samrat-Abdul-Jalil@users.noreply.github.com>
2022-03-17 01:38:51 +0600

    Add files via upload

---
+++ PHP files/Addpateint.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+         SESSION_start();             
+    ?>
+
+
+<?php
+                    if(isset($_SESSION["table"])){
+                    $d=$_SESSION["table"];
+                    $da="general_user";
+                     if($d=$da){
+                        
+                        
+                         ?>
+  <head>
+        <meta charset="UTF-8">
+        <title>MEDIFIND</title>
+        <link rel="stylesheet" href="side.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+   
+       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    </head>
+    <body>
+        <input type="checkbox" id="check">
+        <label for="check">
+        <i class="fa fa-bars" id="btn"></i>
+        <i class="fa fa-times" id="cancel"></i>
+        </label>
+    <div class="sidebar">
+    <a href="index.php"><header>MEDIFIND</header></a>
+         <ul>
+             <li><a href="DashboardAdmin.php"><i class="fa fa-qrcode"></i><?php echo $_SESSION["username"]?></a></li>
+             <li><a href="#"><i class="fa fa-shopping-bag"></i>ADD pateint</a></li>
+             <li><a href="#"><i class="fa fa-plus-square"></i>complain shop</a></li>
+            
+             <li><a href="Logout.php"><i class="fa fa-tasks"></i>LogOut</a></li>
+        
+            
+             
+        </ul>
+        </div>
+        <section>
+        <form action="Addpateint.php" method="get">
+                           <div class="row"> <div class="col-4"><input type = "text"name="Name" class="input-box" placeholder="Name" required></div>   </div>  
+                           <div class="row"> <div class="col-4"> <input type = "text"name="age " class="input-box" placeholder="age" required></div>   </div>
+                           <div class="row"> <div class="col-4"> <input type = "text"name="date" class="input-box" placeholder="date Of prescription" required></div>   </div>
+                           <div class="row"> <div class="col-4"><input type = "Password"name="Doctor"  class="input-box" placeholder="Doctor Name" required></div>   </div>
+                           <div class="row"> <div class="col-4"><label for="exampleInputEmail1" class="form-label">Prescription</label>
+                           
+                            <div class="col-4"><textarea name="prescrption" rows="8"col="70"placeholder="Prescription" > </textarea>
+   <div class="col-4"><button type="submit" class="btn btn-dark">Add Pateint</button> </div>   </div>
+                           
+                         </form>
+
+        </section>
+        
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col-1">
+                        <h3>Download Our App</h3>
+                        <p>Download App for Android and ios Mobile Phone.</p>
+                        <div class="app-logo">
+                          <img src="images/play-store.png">
+                            <img src="images/app-store.png">
+                        
+                        
+                        </div>
+                        
+                    </div>
+                    
+                     <div class="footer-col-2">
+                        <img src="images/M.png" width="200px">
+                        <p>To provide safe, high quality patient care in an atmosphere <br>of professionalism, respect, and effective communication.</p>
+                    </div>
+                    
+                    
+                     <div class="footer-col-3">
+                        <h3>Useful Links</h3>
+                         <ul>
+                             <li>Coupons</li>
+                             <li>Blog Post</li>
+                             <li>Return Policy</li>
+                             <li>Join Affilicate</li>
+                         
+                         </ul>
+                    </div>
+                    
+                    
+                    
+                    <div class="footer-col-4">
+                        <h3>Follow Us</h3>
+                         <ul>
+                             <li>Facebook</li>
+                             <li>Twitter</li>
+                             <li>Instagram</li>
+                             <li>YouTube</li>
+                         
+                         </ul>
+                    </div>
+                
+                
+                </div>
+            
+                
+                
+                <hr>
+                <p class="copyright"> Copyright 2020- easy tutorial</p>
+                
+                </div>
+        
+        
+        
+        </div>
+        
+    </body>
+
+                      
+                         <?php 
+                     }} else{
+                         ?>
+                         <br><br><br><br><br><br><br><br>
+                         <br><br>
+                         <br><br>
+                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      
+                         <div class="alert alert-danger" role="alert">
+                                   <h1> Access Denied</h>
+                                            </div>
+
+                            <?php 
+
+                     }
+                         ?>
+
+
+
+  
+</html>
