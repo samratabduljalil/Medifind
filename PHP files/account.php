01d22ef6982072b6ad08b30987a6f87eb7c2b430
Samrat Abdul Jalil <95965521+Samrat-Abdul-Jalil@users.noreply.github.com>
2022-03-17 01:36:28 +0600

    Add files via upload

---
+++ PHP files/account.php
@@ -0,0 +1,45 @@
+
+
+
+
+
+<html>
+    <head>
+        <title> Account
+        </title>
+        <link rel="stylesheet" href="login.css">
+    </head>
+    
+     <body>
+
+         
+         
+         <div class="container">
+             <div class="card">
+                 <div class="inner-box" id="card">
+                     <div class="card-front">
+                         <h2>Select  </h2>
+                       <a href="loginuser.php" > <button type = "Submit" class="submit-btn"> General User  </button></a>
+                       <a href="loginadmin.php" ><button type = "Submit" class="submit-btn"> Admin </button></a>
+                       <a href="login.php" ><button type = "Submit" class="submit-btn"> Medical Shop</button></a>
+                       <a href="logincompany.php" ><button type = "Submit" class="submit-btn"> Medicine Company </button></a>
+                     
+                        
+                              
+                         </form>
+                     
+
+
+                     
+                     </div>
+                 </div>
+             </div>
+         </div>
+        
+       
+  
+         
+         
+         
+</body>    
+</html>
